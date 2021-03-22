c137041906e4b100276694c2e6c22163da7eb58a
meghamodi <meghamodi007@gmail.com>
2021-03-22 02:25:26 -0500

    dashboard dynamic

--- btre/settings.py
+++ btre/settings.py
@@ -145,7 +145,9 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# try:
-#     from .local_settings import *
-# except ImportError:
-#     pass
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'meghamodi007@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
